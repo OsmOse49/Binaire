6be8132dfb075159474529e9d51c845ed7695898
OsmOse49 <rayan.ben-hamida@etu.univ-lyon1.fr>
2025-03-21 08:59:00 +0000

    Delete header.h

--- header.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <cmath>
-
-
-void calcul(double x, double y, double &ax, double &ay);
-void retour(double &vx, double &vy, double &x, double &y, double ax, double ay);
-void menu();
